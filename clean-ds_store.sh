5fbf39279825cb7078d566695dd9f6b21a8e495e
theStudio60 <41513752+theStudio60@users.noreply.github.com>
2018-08-18 07:14:46 +0200

    Add files via upload

---
+++ clean-ds_store.sh
@@ -0,0 +1,16 @@
+// theStudio60 - wp-dev-tools
+// https://github.com/theStudio60/wp-dev-tools
+//
+// LOCAL LAB ONLY
+// OS : macos
+//
+// clean .DS_Store metadata folders.
+// usefull before commit 
+// 
+// HOW TO USE ??
+// put this script in the directory of your wp site
+// then in the terminal :
+// cd "wp directory"
+// chmod +x clean-ds_store.sh
+// ./clean-ds_store.sh 
+"find . -type f -name '*.DS_Store' -ls -delete"
