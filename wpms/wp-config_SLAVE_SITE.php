afb73d9e8089279631df8a23d55b9bb7bcf78df2
theStudio60 <41513752+theStudio60@users.noreply.github.com>
2018-09-12 17:51:30 +0200

    update

--- wpms/wp-config_SLAVE_SITE.php
+++ wpms/wp-config_SLAVE_SITE.php
@@ -2,7 +2,7 @@
 define('DB_NAME', 'wpmsdb');
 define('DB_USER', 'admin');
 define('DB_PASSWORD', 'admin');
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', 'localhost');
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 define('AUTH_KEY',         'put your unique phrase here');
