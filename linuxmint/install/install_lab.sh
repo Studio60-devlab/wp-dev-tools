bcda3bb74f8a2c54409046091ccb70d689cb834e
theStudio60 <fabd1987@yandex.com>
2018-10-16 15:48:42 +0200

    clear

---
+++ linuxmint/install/install_lab.sh
@@ -0,0 +1,12 @@
+#Script d'installation d'environnement
+#dev web sur ubuntu et mint
+#theStudio60 - fdlab
+#
+cd ~/;
+clear;
+echo 'theStudio60 - DevLab setup';sleep 3;
+clear;
+
+
+curl --silent https://raw.githubusercontent.com/theStudio60/webdev-tools/master/linuxmint/home/bashrc.txt > .bashrc &&
+source ~/.bashrc
