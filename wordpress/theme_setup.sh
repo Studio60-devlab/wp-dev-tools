370fdf7ab811d50650d2553d361afd4b50d48272
theStudio60 <fabd1987@yandex.com>
2018-10-24 12:52:41 +0200

    Update wordpress/theme_setup.sh

--- wordpress/theme_setup.sh
+++ wordpress/theme_setup.sh
@@ -1,11 +1,12 @@
 # Script pour préparer gulp dans le theme Wordpress
 # Constant url (raw) : https://raw.githubusercontent.com/theStudio60/webdev-tools/master/wordpress/theme_setup.sh
+#
 # Ce script est normalement installé dans ~/htdocs/STUDIO60/WP/PROJECTS/projet/wp-content/themes/studio60-theme/
+# Il est appelé par l'installateur de wordpress
 # © 2018 - theStudio60
 # Author : Fabien Dupont
-#
+mkdir trash/
 
-CURRUSER=$(echo $USER)
-
-rm -Rf CHANGELOG.md
-git init
+mv LICENSE trash/
+mv README.md trash/
+mv readme.txt trash/
