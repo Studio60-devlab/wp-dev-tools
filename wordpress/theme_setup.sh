93b772a32c3bb1c9d529d2ce632dc4a5a41d2d68
theStudio60 <fabd1987@yandex.com>
2018-10-24 16:25:46 +0200

    Update wordpress/theme_setup.sh

--- wordpress/theme_setup.sh
+++ wordpress/theme_setup.sh
@@ -8,23 +8,14 @@
 THEME_DIR=$(pwd)
 echo 'theme directory is $THEME_DIR';
 
+mkdir trash/
 
-trash(){
+
   mv LICENSE trash/
   mv README.md trash/
   mv readme.txt trash/
-};
-node_init(){
+
 #  git init -q
   npm init -y
-};
-gulp_dl(){
   curl --silent https://raw.githubusercontent.com/theStudio60/webdev-tools/master/gulp/gulpfile.js > Gulpfile.js
   npm install gulp gulp-sass gulp-autoprefixer gulp-newer gulp-imagemin browser-sync gulp-concat gulp-uglify gulp-sourcemaps gulp-watch --save-dev
-
-};
-
-mkdir trash/
-trash &&
-node_init &&
-gulp_dl
