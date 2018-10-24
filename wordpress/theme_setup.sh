3adfa70930da220b5039b677584a0343821a8d34
theStudio60 <fabd1987@yandex.com>
2018-10-24 11:59:45 +0200

    add constant url

--- wordpress/theme_setup.sh
+++ wordpress/theme_setup.sh
@@ -1,7 +1,11 @@
 # Script pour préparer gulp dans le theme Wordpress
+# Constant url (raw) : https://raw.githubusercontent.com/theStudio60/webdev-tools/master/wordpress/theme_setup.sh
 # Ce script est normalement installé dans ~/htdocs/STUDIO60/WP/PROJECTS/projet/wp-content/themes/studio60-theme/
 # © 2018 - theStudio60
 # Author : Fabien Dupont
 #
 
 CURRUSER=$(echo $USER)
+
+rm -Rf CHANGELOG.md
+git init
