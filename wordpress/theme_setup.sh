6f4f9c4a159deb8c1878cb04c552a7161b3ff933
theStudio60 <fabd1987@yandex.com>
2018-10-24 16:23:55 +0200

    Update wordpress/theme_setup.sh

--- wordpress/theme_setup.sh
+++ wordpress/theme_setup.sh
@@ -5,14 +5,26 @@
 # Il est appelé par l'installateur de wordpress
 # © 2018 - theStudio60
 # Author : Fabien Dupont
-git init -q
-npm init -y
+THEME_DIR=$(pwd)
+echo 'theme directory is $THEME_DIR';
+
+
+trash(){
+  mv LICENSE trash/
+  mv README.md trash/
+  mv readme.txt trash/
+};
+node_init(){
+#  git init -q
+  npm init -y
+};
+gulp_dl(){
+  curl --silent https://raw.githubusercontent.com/theStudio60/webdev-tools/master/gulp/gulpfile.js > Gulpfile.js
+  npm install gulp gulp-sass gulp-autoprefixer gulp-newer gulp-imagemin browser-sync gulp-concat gulp-uglify gulp-sourcemaps gulp-watch --save-dev
+
+};
 
 mkdir trash/
-mv LICENSE trash/
-mv README.md trash/
-mv readme.txt trash/
-
-
-curl --silent https://raw.githubusercontent.com/theStudio60/webdev-tools/master/gulp/gulpfile.js > gulpfile.js
-npm install gulp gulp-sass gulp-autoprefixer gulp-newer gulp-imagemin browser-sync gulp-concat gulp-uglify gulp-sourcemaps gulp-watch --save-dev
+trash &&
+node_init &&
+gulp_dl
