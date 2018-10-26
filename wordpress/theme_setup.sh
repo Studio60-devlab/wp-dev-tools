74022d32525f1b42a5b08cf88b407b6f3ba204dd
theStudio60 <fabd1987@yandex.com>
2018-10-26 19:44:13 +0200

    wip

--- wordpress/theme_setup.sh
+++ wordpress/theme_setup.sh
@@ -5,18 +5,11 @@
 # Il est appelé par l'installateur de wordpress
 # © 2018 - theStudio60
 # Author : Fabien Dupont
-echo "Avant de commencer"
-echo "régler proxy_url du gulpfile"
-cat proxy_url.info
-
-cat designer.info
-
 sleep 5 ;
 
 clear;
 
 mkdir assets/
-
 mkdir assets/images/
 mkdir assets/images/original/
 
@@ -25,17 +18,33 @@ mkdir assets/js/dev/
 mkdir assets/js/dist/
 
 mkdir assets/sass/
-mkdir assets/sass/_partials/
-
-touch assets/sass/style.scss
+mkdir assets/sass/flex/
+mkdir assets/sass/layout/
+mkdir assets/sass/partials/
 
 curl --silent https://raw.githubusercontent.com/theStudio60/webdev-tools/master/wordpress/gulp/assets/sass/style.scss > assets/sass/style.scss
+curl --silent https://raw.githubusercontent.com/theStudio60/webdev-tools/master/wordpress/gulp/assets/sass/_variables.scss > assets/sass/_variables.scss
+curl --silent https://raw.githubusercontent.com/theStudio60/webdev-tools/master/wordpress/gulp/assets/sass/_styles.scss > assets/sass/_styles.scss
+curl --silent https://raw.githubusercontent.com/theStudio60/webdev-tools/master/wordpress/gulp/assets/sass/_base.scss > assets/sass/_base.scss
+
+curl --silent https://raw.githubusercontent.com/theStudio60/webdev-tools/master/wordpress/gulp/assets/sass/flex/_content.scss > assets/sass/flex/_content.scss
+curl --silent https://raw.githubusercontent.com/theStudio60/webdev-tools/master/wordpress/gulp/assets/sass/flex/_cta.scss > assets/sass/flex/_cta.scss
+curl --silent https://raw.githubusercontent.com/theStudio60/webdev-tools/master/wordpress/gulp/assets/sass/flex/_gallery.scss > assets/sass/flex/_gallery.scss
+curl --silent https://raw.githubusercontent.com/theStudio60/webdev-tools/master/wordpress/gulp/assets/sass/flex/_video.scss > assets/sass/flex/_video.scss
+
+curl --silent https://raw.githubusercontent.com/theStudio60/webdev-tools/master/wordpress/gulp/assets/sass/layout/_footer.scss > assets/sass/layout/_footer.scss
+curl --silent https://raw.githubusercontent.com/theStudio60/webdev-tools/master/wordpress/gulp/assets/sass/layout/_header.scss > assets/sass/layout/_header.scss
+curl --silent https://raw.githubusercontent.com/theStudio60/webdev-tools/master/wordpress/gulp/assets/sass/layout/_menu.scss > assets/sass/layout/_menu.scss
+
+curl --silent https://raw.githubusercontent.com/theStudio60/webdev-tools/master/wordpress/gulp/assets/sass/partials/_btn.scss > assets/sass/partials/_btn.scss
+curl --silent https://raw.githubusercontent.com/theStudio60/webdev-tools/master/wordpress/gulp/assets/sass/partials/_hero.scss > assets/sass/partials/_hero.scss
+curl --silent https://raw.githubusercontent.com/theStudio60/webdev-tools/master/wordpress/gulp/assets/sass/partials/_tile.scss > assets/sass/partials/_tile.scss
+
 
 mkdir trash/
   mv LICENSE trash/
   mv README.md trash/
   mv readme.txt trash/
-
   mv sass/ trash/
   mv layouts/ trash/
   mv js/ trash/
