b8e7374a0f32963fb92c6c14f2f574cacaa358d3
theStudio60 <fabd1987@yandex.com>
2018-10-27 06:52:59 +0200

    no postcss

--- wordpress/theme_setup.sh
+++ wordpress/theme_setup.sh
@@ -62,7 +62,7 @@ echo
 echo "* Maintenant Node entre en scene..."
 echo
 npm init -y
-npm install gulp gulp-sass gulp-autoprefixer gulp-newer gulp-imagemin browser-sync gulp-concat gulp-uglify gulp-sourcemaps gulp-watch --save-dev
+npm install gulp gulp-sass gulp-autoprefixer gulp-newer gulp-imagemin browser-sync gulp-concat gulp-uglify gulp-sourcemaps gulp-watch bootstrap-sass --save-dev
 echo
 echo "* Taper gulp pour lancer le serveur dev"
 echo
