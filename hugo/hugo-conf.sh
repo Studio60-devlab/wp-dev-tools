b0df1450f9ed3d242f25cd27b909aa4aa6d2ad06
theStudio60 <fab.d1987@yandex.com>
2018-10-31 11:47:59 +0100

    Update hugo/hugo-conf.sh

--- hugo/hugo-conf.sh
+++ hugo/hugo-conf.sh
@@ -4,14 +4,49 @@
 # © 2018 - theStudio60
 # Author : Fabien Dupont
 #
-#CURRUSER=$(echo $USER)
-
 ls
+
+git init -q
+touch .gitignore
+echo "designer.info" >> .gitignore
+echo >> .gitignore
+
+echo "node_modules" >> .gitignore
+echo >> .gitignore
+
+echo "public" >> .gitignore
+echo >> .gitignore
+
+
 touch archetypes/articles.md
+
 echo "+++" >> archetypes/articles.md
+echo "" >>  archetypes/articles.md
 echo "Author = \"Studio60\"" >> archetypes/articles.md
 echo "Description = \"\"" >> archetypes/articles.md
 echo "resources = []" >> archetypes/articles.md
 echo "slug = \"\"" >> archetypes/articles.md
 echo "" >>  archetypes/articles.md
 echo "+++" >> archetypes/articles.md
+
+touch layouts/articles/single.html
+
+echo "<!DOCTYPE html> " >> layouts/articles/single.html 
+echo "<html>" >> layouts/articles/single.html 
+echo "<title>{{ .Title }}</title>" >> layouts/articles/single.html 
+echo "<meta charset=\"UTF-8\">" >> layouts/articles/single.html 
+echo "<meta name=\"description\" content=\"{{ .Description }}\">" >> layouts/articles/single.html 
+echo "</head>" >> layouts/articles/single.html 
+echo "<body>" >> layouts/articles/single.html 
+echo "<main>" >> layouts/articles/single.html 
+echo "<article>" >> layouts/articles/single.html 
+echo "<h1>" >> layouts/articles/single.html 
+echo "{{ .Title }}" >> layouts/articles/single.html 
+echo "</h1>" >> layouts/articles/single.html 
+echo "{{ .Content }}" >> layouts/articles/single.html 
+echo "</article>" >> layouts/articles/single.html 
+echo "</main>" >> layouts/articles/single.html 
+echo "</body>" >> layouts/articles/single.html 
+echo "</html>" >> layouts/articles/single.html 
+
+hugo -vw
