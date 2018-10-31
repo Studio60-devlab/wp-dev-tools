ca27011e99fed415874257d45eb3b8d37fa14ca3
theStudio60 <fab.d1987@yandex.com>
2018-10-31 10:32:37 +0100

    Update hugo/hugo-conf.sh

---
+++ hugo/hugo-conf.sh
@@ -0,0 +1,17 @@
+# studio60 -
+# Script pour configurer un nouveau projet hugo
+# Ce script a été copié/téléchargé par l'installeur hugo-install.sh 
+# © 2018 - theStudio60
+# Author : Fabien Dupont
+#
+#CURRUSER=$(echo $USER)
+
+ls
+touch archetypes/articles.md
+echo "+++" >> archetypes/articles.md
+echo "Author = \"Studio60\"" >> archetypes/articles.md
+echo "Description = \"\"" >> archetypes/articles.md
+echo "resources = []" >> archetypes/articles.md
+echo "slug = \"\"" >> archetypes/articles.md
+echo "" >>  archetypes/articles.md
+echo "+++" >> archetypes/articles.md
