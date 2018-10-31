52a8f75100ebd0cceb1b212e3df09b78bfbf6640
theStudio60 <fab.d1987@yandex.com>
2018-10-31 13:27:51 +0100

    script in progress

--- hugo/hugo-conf.sh
+++ hugo/hugo-conf.sh
@@ -1,52 +1,58 @@
 # studio60 -
 # Script pour configurer un nouveau projet hugo
-# Ce script a été copié/téléchargé par l'installeur hugo-install.sh 
+# Ce script a été copié/téléchargé par l'installeur hugo-install.sh
 # © 2018 - theStudio60
 # Author : Fabien Dupont
 #
 ls
 
+touch archetypes/default.md
+
+echo "---" >> archetypes/default.md
+echo "" >>  archetypes/default.md
+echo "Author = \"Studio60\"" >> archetypes/default.md
+echo "Description = \"\"" >> archetypes/default.md
+echo "resources = []" >> archetypes/default.md
+echo "slug = \"\"" >> archetypes/default.md
+echo "" >>  archetypes/default.md
+echo "---" >> archetypes/default.md
+
+mkdir layouts/_default
+touch layouts/index.html
+touch layouts/404.html
+
+echo "<!DOCTYPE html> " >> layouts/index.html
+echo "<html>" >> layouts/index.html
+echo "<title>{{ .Title }}</title>" >> layouts/index.html
+echo "<meta charset=\"UTF-8\">" >> layouts/index.html
+echo "<meta name=\"description\" content=\"{{ .Description }}\">" >> layouts/index.html
+echo "</head>" >> layouts/index.html
+echo "<body>" >> layouts/index.html
+echo "<main>" >> layouts/index.html
+echo "<article>" >> layouts/index.html
+echo "<h1>" >> layouts/index.html
+echo "{{ .Title }}" >> layouts/index.html
+echo "</h1>" >> layouts/index.html
+echo "{{ .Content }}" >> layouts/index.html
+echo "</article>" >> layouts/index.html
+echo "</main>" >> layouts/index.html
+echo "</body>" >> layouts/index.html
+echo "</html>" >> layouts/index.html
+
 git init -q
 touch .gitignore
 echo "designer.info" >> .gitignore
 echo >> .gitignore
-
+echo "hugo-conf.sh" >> .gitignore
+echo >> .gitignore
+echo "gulpfile.js" >> .gitignore
+echo >> .gitignore
 echo "node_modules" >> .gitignore
 echo >> .gitignore
-
-echo "public" >> .gitignore
+echo "public/" >> .gitignore
 echo >> .gitignore
-
-
-touch archetypes/articles.md
-
-echo "+++" >> archetypes/articles.md
-echo "" >>  archetypes/articles.md
-echo "Author = \"Studio60\"" >> archetypes/articles.md
-echo "Description = \"\"" >> archetypes/articles.md
-echo "resources = []" >> archetypes/articles.md
-echo "slug = \"\"" >> archetypes/articles.md
-echo "" >>  archetypes/articles.md
-echo "+++" >> archetypes/articles.md
-
-touch layouts/articles/single.html
-
-echo "<!DOCTYPE html> " >> layouts/articles/single.html 
-echo "<html>" >> layouts/articles/single.html 
-echo "<title>{{ .Title }}</title>" >> layouts/articles/single.html 
-echo "<meta charset=\"UTF-8\">" >> layouts/articles/single.html 
-echo "<meta name=\"description\" content=\"{{ .Description }}\">" >> layouts/articles/single.html 
-echo "</head>" >> layouts/articles/single.html 
-echo "<body>" >> layouts/articles/single.html 
-echo "<main>" >> layouts/articles/single.html 
-echo "<article>" >> layouts/articles/single.html 
-echo "<h1>" >> layouts/articles/single.html 
-echo "{{ .Title }}" >> layouts/articles/single.html 
-echo "</h1>" >> layouts/articles/single.html 
-echo "{{ .Content }}" >> layouts/articles/single.html 
-echo "</article>" >> layouts/articles/single.html 
-echo "</main>" >> layouts/articles/single.html 
-echo "</body>" >> layouts/articles/single.html 
-echo "</html>" >> layouts/articles/single.html 
-
-hugo -vw
+echo "static/" >> .gitignore
+echo >> .gitignore
+#build :
+# gulp build
+# hugo
