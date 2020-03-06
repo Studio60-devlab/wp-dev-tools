1c45c1cc8d3d2cd9c2c883134b1c4e3aab3d319d
Fabien Dupont <fab.d1987@yandex.com>
2020-03-06 17:49:51 +0100

    Create wp-cli_install.MACOS.sh

---
+++ installers/macos/wp-cli_install.MACOS.sh
@@ -0,0 +1,19 @@
+########################## 
+# This is an installer for wp-cli
+# So you MUST have PHP installed (MAMP).
+#
+# usage :
+# chmod +x wp-cli_install.MACOS.sh ; ./wp-cli_install.MACOS.sh
+#
+# Author : Fabien Dupont
+# https://github.com/fabien-dupont
+#
+########################## 
+clear ;
+echo 'Downloading wp-cli...' &&
+curl  -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar &&
+php wp-cli.phar --info &&
+chmod +x wp-cli.phar &&
+mv wp-cli.phar /usr/local/bin/wp && 
+echo 'Testing...' &&
+wp --info &&
